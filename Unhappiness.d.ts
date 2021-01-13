32de1f1e1678605f3f7b4e1321407e0f17fc1c61
Dom Hastings <dom111@users.noreply.github.com>
2021-01-13 20:38:52 +0000

    Initial commit

---
+++ Unhappiness.d.ts
@@ -0,0 +1,3 @@
+import Yield from '@civ-clone/core-yield/Yield';
+export declare class Unhappiness extends Yield {}
+export default Unhappiness;
---
+++ Unhappiness.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Unhappiness = void 0;
+const Yield_1 = require("@civ-clone/core-yield/Yield");
+class Unhappiness extends Yield_1.default {
+}
+exports.Unhappiness = Unhappiness;
+exports.default = Unhappiness;
+//# sourceMappingURL=Unhappiness.js.map
